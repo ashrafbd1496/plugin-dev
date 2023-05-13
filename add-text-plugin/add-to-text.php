2e045d7699a84212e370c951542b9cde853c01d2
ashrafbd1496 <asraf1496@gmail.com>
2023-05-13 23:48:44 +0600

    added plugin header

---
+++ add-text-plugin/add-to-text.php
@@ -0,0 +1,16 @@
+<?php 
+/*
+ * Plugin Name:       Add To Text Plugin
+ * Plugin URI:        https://github.com/ashrafbd1496/plugin-dev
+ * Description:       This plugin willl add text at bottom of every post
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Md Ashraf Uddin
+ * Author URI:        https://ashrafbd.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/ashrafbd1496/plugin-dev
+ * Text Domain:       adt-plugin
+ * Domain Path:       /languages
+ */
