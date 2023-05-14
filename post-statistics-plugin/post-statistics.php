e42a805fa41d5beedf95d20ca39c4c9bba93261a
ashrafbd1496 <asraf1496@gmail.com>
2023-05-14 12:48:02 +0600

    post statistics plugin initialize

--- post-statistics-plugin/post-statistics.php
+++ post-statistics-plugin/post-statistics.php
@@ -1,6 +1,6 @@
 <?php 
 /*
- * Plugin Name:       Post Statistics
+ * Plugin Name:       Add To Text Plugin
  * Plugin URI:        https://github.com/ashrafbd1496/plugin-dev
  * Description:       This plugin willl add text at bottom of every post
  * Version:           1.0.0
@@ -11,9 +11,18 @@
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Update URI:        https://github.com/ashrafbd1496/plugin-dev
- * Text Domain:       pstatistics
+ * Text Domain:       adt-plugin
  * Domain Path:       /languages
  */
 
 
- 
+ add_filter('the_content','add_to_end_of_the_post' );
+
+ function add_to_end_of_the_post($content){
+    if(is_single() && is_main_query()){
+        return $content . '<h3>This text has added using plugin</h3>';
+    }
+
+    return $content;
+   
+ }
